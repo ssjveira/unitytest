d69e91a87375ac16b835b1b090f5a0ddec5972b6
Robert Veira <robert.veira@knights.ucf.edu>
2019-05-31 12:14:15 -0400

    Modified PlayerController to properly initialize all its member variables.

--- UnityTest/Assets/Scripts/PlayerController.cs
+++ UnityTest/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,10 @@ public class PlayerController : MonoBehaviour
     private float speed = 0.0f;
 
     [SerializeField]
-    private Text m_CountText;
+    private Text m_CountText = null;
 
     [SerializeField]
-    private Text m_WinText;
+    private Text m_WinText = null;
 
     private Rigidbody m_rigidBody = null;
     private int m_count = 0;
