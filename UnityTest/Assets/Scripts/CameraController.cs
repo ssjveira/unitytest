bb7cd7a18d98fbd66a54bf876b2bb7e4331728c0
Robert Veira <robert.veira@knights.ucf.edu>
2019-05-31 01:02:31 -0400

    Updated UnityTest app to create the roll-a-ball game from the Unity Learn website.

---
+++ UnityTest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_Player = null;
+
+    private Vector3 m_Offset = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Offset = transform.position - m_Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = m_Player.transform.position + m_Offset;
+    }
+}
---
+++ UnityTest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0.0f;
+
+    [SerializeField]
+    private Text m_CountText;
+
+    [SerializeField]
+    private Text m_WinText;
+
+    private Rigidbody m_rigidBody = null;
+    private int m_count = 0;
+
+    private void Start()
+    {
+        m_count = 0;
+        m_rigidBody = GetComponent<Rigidbody>();
+        SetCountText();
+        m_WinText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        m_rigidBody.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    private void SetCountText()
+    {
+        m_CountText.text = "Count: " + m_count.ToString();
+        if(m_count >= 12)
+        {
+            m_WinText.text = "You win!";
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            m_count++;
+            SetCountText();
+        }
+    }
+}
